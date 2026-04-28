7131114d65e974ce997b9a85673c572cd559560a
Lavashina <Hitodena@ya.ru>
2026-04-28 09:16:35 +0300

    clean: clean all old data

--- backup-root/start-dev.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting SupplierFinder development server...
-cd /d "%~dp0SupplierFinder"
-echo Current directory: %CD%
-echo Running npm run dev...
-npm run dev
-pause
--- checkpoint_file.tsx
+++
@@ -1,1 +0,0 @@
-hell -Command git\ show\ bd546478db8fd33a66bd5a389920b2ec886326c2:client/src/pages/request-details-new.tsx
